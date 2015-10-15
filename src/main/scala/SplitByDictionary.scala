08ce26c9f5e84969b67c104a6da25b365fd87dbd
Tagir <tagir.magomedov@gmail.com>
2015-10-14 19:02:13 -0700

    Split by dictionary

---
+++ src/main/scala/SplitByDictionary.scala
@@ -0,0 +1,30 @@
+/*
+Given dictionary of words find if input string can be split into words from that dictionary.
+
+Input:
+["hello", "world"]
+helloworld
+
+Output:
+true
+*/
+
+object SplitByDictionary extends App {
+
+  val dictionary = List("hello", "hell", "o", "world")
+
+  // solution
+  def chain(input: String): List[List[String]] =
+    if (input.isEmpty) List(List.empty[String])
+    else for {
+      prefix <- dictionary if input.startsWith(prefix)
+      rest <- chain(input.stripPrefix(prefix))
+    } yield prefix :: rest
+
+  def canSplit(input: String): Boolean =
+    chain(input).exists(c => c.map(_.size).sum == input.size)
+
+  // test
+  println(chain("helloworld"))
+  println(canSplit("helloworld"))
+}
