99209b0bac266ef1f1aa39bcdc7772e90ed9e5a0
Tagir <tagir.magomedov@gmail.com>
2015-10-16 01:05:12 -0700

    Parentheses check

---
+++ src/main/scala/ParenthesesCheck.scala
@@ -0,0 +1,24 @@
+/*
+Check if a given string contains correct parentheses
+*/
+
+object ParenthesesCheck extends App {
+
+  // solution
+  def check(cs: List[Char], n: Int): Boolean =
+    if (n < 0) false
+    else cs match {
+      case Nil => n == 0
+      case c :: tail =>
+        if (c == '(') check(tail, n + 1)
+        else if (c == ')') check(tail, n - 1)
+        else check(tail, n)
+    }
+
+  def check(s: String): Boolean = check(s.toList, 0)
+
+  // test
+  println(check("(()")) // false
+  println(check("()()")) // true
+  println(check("()))")) // false
+}
