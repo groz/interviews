cb0a908a0a62fb1b4f43c63c23d88e1b90bbc722
Tagir <tagir.magomedov@gmail.com>
2015-10-16 20:22:57 -0700

    Needle in haystack.

---
+++ src/main/scala/NeedleInHaystack.scala
@@ -0,0 +1,31 @@
+/**
+Find the first occurence of needle in haystack
+  */
+
+object NeedleInHaystack extends App {
+
+  // ugly and not properly tested
+  // solution
+  def find(needle: String, haystack: String, start: Int, nPos: Int, hPos: Int): Int =
+    (nPos, hPos) match {
+      case (nLen, _) if nLen == needle.length => start
+      case (_, hLen) if hLen == haystack.length => -1
+
+      case _ if start != -1 => // inside a match
+        if (needle(nPos) == haystack(hPos))
+          find(needle, haystack, start, nPos + 1, hPos + 1)
+        else
+          find(needle, haystack, -1, 0, hPos - nPos + 1)
+
+      case _ => // outside a match
+        if (needle(nPos) == haystack(hPos))
+          find(needle, haystack, hPos, 1, hPos + 1)
+        else
+          find(needle, haystack, -1, 0, hPos + 1)
+    }
+
+  def find(needle: String, haystack: String): Int = find(needle, haystack, -1, 0, 0)
+
+  // test
+  println(find("abc", "aaabcabcdabc"))
+}
