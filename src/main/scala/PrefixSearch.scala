8f5d92e3c6fca3a5cdf741daba306e1a71ba9ab3
Tagir <tagir.magomedov@gmail.com>
2015-10-18 00:47:10 -0700

    Immutable prefix search tree

--- src/main/scala/PrefixSearch.scala
+++ src/main/scala/PrefixSearch.scala
@@ -1,68 +1,48 @@
 /*
-Implement fast prefix search for a given dictionary.
-
-warning: below implementation is based on mutable stuff and probably overcomplicated
+Implement prefix search data structure for a given dictionary.
 */
-
-import scala.collection.mutable
 
 object PrefixSearch extends App {
 
   // solution
-  class LookupTree(val c: Option[Char], val children: mutable.Set[LookupTree]) {
+  case class LookupTree(node: Map[Char, LookupTree]) {
+
+    def add(word: String, pos: Int = 0): LookupTree =
+      if (pos == word.size) this
+      else {
+        val char = word(pos)
+        val next = node.getOrElse(char, LookupTree.empty)
+        LookupTree(node.updated(char, next.add(word, pos + 1)))
+      }
 
     def contains(prefix: String, pos: Int = 0): Boolean =
-      if (pos == prefix.size) true
-      else {
-        val next = children.find(_.c.exists(_ == prefix(pos)))
-        next.fold(false)(_.contains(prefix, pos + 1))
-      }
+      (pos == prefix.size) || node.get(prefix(pos)).fold(false){ _.contains(prefix, pos + 1) }
 
-    def findLast(prefix: String, pos: Int = 0): Option[LookupTree] =
+    def findNode(prefix: String, pos: Int = 0): Option[LookupTree] = {
       if (pos == prefix.size) Some(this)
-      else {
-        val next = children.find(_.c.exists(_ == prefix(pos)))
-        next.fold(None: Option[LookupTree])(_.findLast(prefix, pos + 1))
-      }
-
-    def search(prefix: String): Set[String] =
-      findLast(prefix).fold(Set.empty[String])(_.all(Set(prefix)))
-
-    def all(result: Set[String]): Set[String] = {
-      if (children.isEmpty) result
-      else for {
-        w: String <- result
-        rest <- children.flatMap(ch => ch.all(result.map(_ + ch.c.get)))
-      } yield rest
+      else node.get(prefix(pos)).fold(None: Option[LookupTree])(_.findNode(prefix, pos + 1))
     }
 
-    override def toString = s"${c.getOrElse('.')} -> ${children.mkString("[", ",", "]")}"
+    def search(prefix: String): Set[String] = findNode(prefix).fold(Set.empty[String])(_.build(Set(prefix)))
+
+    def build(from: Set[String]): Set[String] =
+      if (node.isEmpty) from
+      else for {
+        s <- from
+        (char, child) <- node
+        w <- child.build(from.map(_ + char))
+      } yield w
   }
 
-  def createLookup(dictionary: Set[String]): LookupTree = {
+  object LookupTree {
+    val empty = LookupTree(Map.empty[Char, LookupTree])
 
-    val lookupTree = new LookupTree(None, mutable.Set.empty[LookupTree])
-
-    for (word <- dictionary) {
-      var currentLookup = lookupTree
-
-      for (c <- word) {
-        val node: Option[LookupTree] = currentLookup.children.find(_.c.exists(_ == c))
-
-        currentLookup = node.fold {
-          val newTree = new LookupTree(Some(c), mutable.Set.empty[LookupTree])
-          currentLookup.children.add(newTree)
-          newTree
-        } { identity }
-      }
-    }
-
-    lookupTree
+    def apply(dictionary: Set[String]): LookupTree =
+      dictionary.foldLeft(LookupTree.empty)((acc, w) => acc.add(w))
   }
 
   // test
-  val lookup = createLookup(Set("hello", "held", "world", "bye", "holding"))
-  println(lookup)
+  val lookup = LookupTree(Set("bye", "held", "hell", "world", "woo"))
 
   println(lookup.contains("hel"))
   println(lookup.contains("helloo"))
