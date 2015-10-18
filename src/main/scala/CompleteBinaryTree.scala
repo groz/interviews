70159f5ddb5a43ee416c34cb8a2184164db726ee
Tagir <tagir.magomedov@gmail.com>
2015-10-17 19:57:54 -0700

    Complete binary tree

---
+++ src/main/scala/CompleteBinaryTree.scala
@@ -0,0 +1,42 @@
+/*
+Check if the binary tree is complete.
+In a complete binary tree every level, except possibly the last, is completely filled,
+and all nodes in the last level are as far left as possible.
+*/
+
+object CompleteBinaryTree extends App {
+
+  val branchFactor = 2
+
+  // solution
+  case class Tree(children: Seq[Tree])
+
+  def nextLevel(nodes: Seq[Tree]) = nodes.flatMap(_.children)
+
+  def isComplete(nodes: Seq[Tree], level: Int): Boolean = {
+    val next = nextLevel(nodes)
+
+    if (next.isEmpty) {
+      // last level, check that all full nodes are to the left
+      val (full, notfull) = nodes.span(_.children.size == branchFactor)
+      notfull.forall(_.children.size < branchFactor)
+    } else {
+      nodes.size == Math.pow(branchFactor, level) && isComplete(next, level + 1)
+    }
+  }
+
+  def isComplete(root: Tree): Boolean = isComplete(Seq(root), 0)
+
+  // test
+  val leaf = Tree(Seq.empty)
+  val complete = Tree( Seq(leaf, leaf) )
+
+  val noncomplete = Tree( Seq(
+    Tree(Seq(Tree(Seq(leaf)), leaf)),
+    leaf
+  ))
+
+  println(isComplete(leaf))
+  println(isComplete(complete))
+  println(isComplete(noncomplete))
+}
